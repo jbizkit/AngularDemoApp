23f69a58a847531ab8605d026307c91d18d3f04e
jonahpereira <jonahp1990@gmail.com>
2014-07-26 20:03:30 +0530

    Initial

---
+++ AngularDemoApp/AngularDemoApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AngularDemoApp.Startup))]
+namespace AngularDemoApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
